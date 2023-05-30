80f68cff9d4a5d12519308aff25d18048879f482
Goleta12 <giogoletiani1999@gmail.com>
2023-05-30 16:41:25 +0400

    gakvetili3

---
+++ lesson3.py
@@ -0,0 +1,107 @@
+
+
+#if True:
+#    print("zzz")
+#if "g" in my_name:
+#    print("sheicavs g-s")
+
+                   # saintereso gakvetili
+
+#my_name = "giorgi"
+#my_surname = "goletianii"
+#my_age = 23
+#my_sentence = "32 {0} 25 {2} 11 {1}".format(my_name, my_surname, my_age)
+
+#print(my_sentence)
+
+
+                     # .format igulisxmeba damateba gaertianeba ert printshi
+
+
+
+
+              # UAGRESAD SAINTERESO
+
+
+#my_name = "giorgi "
+#my_surname = "qutaishi da dzalian"
+#my_surname2 = "programireba"
+
+
+
+#my_age = "var 23 wlis"
+
+#my_sentence = "chemi saxeli {} me {} vcxovrob {} maintresebs {}".format(my_name, my_age, my_surname, my_surname2)
+
+#print(my_sentence)
+
+
+                                # if , else mnishvneloba 
+
+#my_name = "tamriko"gio
+
+#if "t" in my_name:
+     # print("sheicavs ts")
+#else:
+     #print("ar sheicavs e-s")
+
+#input
+#output          informaciis shetana da gamotana
+
+#my_name = input("enter your name: ")
+
+#print("chemi saxelia " + my_name)
+
+#my_age = 22
+#my_age += 3
+#print(my_age)
+
+#my_name = input("enter your name: ")
+
+#print("chemi saxelia " + my_name)
+
+
+
+
+                                
+
+#my_name = input("enter your name: ")
+#my_susarname = input("enter your usarname: ")
+#my_age = int(input("enter your age: "))
+
+#print("my name is {} my surname is {} my age is {}".format(my_name, my_susarname, my_age))
+
+#input it yoveltvis shemodis terminalshi stringi
+
+#new_age = int(my_age) + 3
+#print("after 3 years i am now {} years old".format(new_age))
+
+if 10>5:
+    print("siyvaruli")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
